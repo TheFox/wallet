c4ed8e7b142f85787766ea273748ee880920b58c
Christian Mayer <christian@fox21.at>
2015-07-05 18:52:09 +0200

    attr_reader added.

--- lib/wallet/entry.rb
+++ lib/wallet/entry.rb
@@ -3,14 +3,14 @@ module Wallet
 	
 	class Entry
 		
+		attr_reader :date
+		attr_reader :amount
+		attr_reader :category
+		
 		def initialize(date = DateTime.now.to_date, amount = 0, category = 'default')
 			self.date = date
 			self.amount = amount
 			@category = category
-		end
-		
-		def date
-			@date
 		end
 		
 		def date=(date)
@@ -25,19 +25,11 @@ module Wallet
 			end
 		end
 		
-		def amount
-			@amount
-		end
-		
 		def amount=(amount)
 			if !amount.is_a?(Fixnum) && !amount.is_a?(Float)
 				raise ArgumentError, 'amount (' + amount.class.to_s + ') must be a Fixnum or a Float'
 			end
 			@amount = amount
-		end
-		
-		def category
-			@category
 		end
 		
 		def to_h
