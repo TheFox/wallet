581cd2e2cc65911e12ddb6baa7357505e7d6dcd7
Christian Mayer <christian@fox21.at>
2015-07-05 19:21:37 +0200

    Entry base functions. #RN

--- lib/wallet/entry.rb
+++ lib/wallet/entry.rb
@@ -3,19 +3,40 @@ module Wallet
 	
 	class Entry
 		
+		attr_reader :title
 		attr_reader :date
-		attr_reader :amount
+		attr_reader :revenue
+		attr_reader :expense
+		attr_reader :balance
 		attr_reader :category
 		
-		def initialize(date = DateTime.now.to_date, amount = 0, category = 'default')
+		def initialize(title = '', date = DateTime.now.to_date, revenue = 0.0, expense = 0.0, category = 'default')
+			revenue_t = revenue.to_f
+			expense_t = expense.to_f
+			
+			self.title = title
 			self.date = date
-			self.amount = amount
+			
+			@revenue = 0.0
+			@expense = 0.0
+			@balance = 0.0
+			
+			if revenue_t < 0 && expense_t == 0
+				self.revenue = 0.0
+				self.expense = revenue_t
+			else
+				self.revenue = revenue_t
+				self.expense = expense_t
+			end
+			
 			@category = category
 		end
 		
+		def title=(title)
+			@title = title.to_s
+		end
+		
 		def date=(date)
-			#puts 'set date: ' + date.class.to_s
-			
 			if date.is_a? String
 				@date = Date.parse(date)
 			elsif date.is_a? Date
@@ -25,30 +46,48 @@ module Wallet
 			end
 		end
 		
-		def amount=(amount)
-			if !amount.is_a?(Fixnum) && !amount.is_a?(Float)
-				raise ArgumentError, 'amount (' + amount.class.to_s + ') must be a Fixnum or a Float'
+		def revenue=(revenue)
+			revenue_t = revenue.to_f
+			
+			if revenue_t < 0
+				raise RangeError, 'revenue (' + revenue_t.to_s + ') cannot be < 0. use expense instead!'
 			end
 			
-			@amount = amount
+			@revenue = revenue_t
+			calc_balance()
+		end
+		
+		def expense=(expense)
+			expense_t = expense.to_f
+			
+			if expense_t > 0
+				raise RangeError, 'expense (' + expense_t.to_s + ') cannot be > 0. use revenue instead!'
+			end
+			
+			@expense = expense_t
+			calc_balance()
 		end
 		
 		def category=(category)
-			if !category.is_a? String
-				raise ArgumentError, 'category must be a String'
-			end
-			
-			@category = category
+			@category = category.to_s
 		end
 		
 		def to_h
 			{
+				'title' => @title,
 				'date' => @date.to_s,
-				'amount' => @amount,
+				'revenue' => @revenue,
+				'expense' => @expense,
+				'balance' => @balance,
 				'category' => @category,
 			}
 		end
 		
+		private
+		
+		def calc_balance
+			@balance = (@revenue.round(3) + @expense.round(3)).to_f.round(3)
+		end
 	end
 	
 end
--- tests/tc_entry.rb
+++ tests/tc_entry.rb
@@ -11,6 +11,14 @@ class TestEntry < MiniTest::Test
 		assert_equal('Wallet::Entry', entry.class.to_s)
 	end
 	
+	def test_set_title
+		entry = Wallet::Entry.new('test1')
+		assert_equal('test1', entry.title)
+		
+		entry.title = 'test2'
+		assert_equal('test2', entry.title)
+	end
+	
 	def test_set_date
 		now = DateTime.now.to_date
 		
@@ -18,14 +26,20 @@ class TestEntry < MiniTest::Test
 		assert_equal('Date', entry.date.class.to_s)
 		assert_equal(now.to_s, entry.date.to_s)
 		
-		entry = Wallet::Entry.new('2015-02-21', 20)
+		entry = Wallet::Entry.new('test', '2015-02-21')
 		assert_equal('Date', entry.date.class.to_s)
 		assert_equal('2015-02-21', entry.date.to_s)
 		
 		entry.date = '2015-01-01'
 		assert_equal('2015-01-01', entry.date.to_s)
 		
-		entry = Wallet::Entry.new(Date.parse('2015-01-02'))
+		entry = Wallet::Entry.new('test', '2014-2-21')
+		assert_equal('2014-02-21', entry.date.to_s)
+		
+		entry.date = '2013-1-1'
+		assert_equal('2013-01-01', entry.date.to_s)
+		
+		entry = Wallet::Entry.new('test', Date.parse('2015-01-02'))
 		assert_equal('2015-01-02', entry.date.to_s)
 		
 		entry = Wallet::Entry.new
@@ -33,40 +47,80 @@ class TestEntry < MiniTest::Test
 		assert_equal(now.to_s, entry.date.to_s)
 		
 		assert_raises(ArgumentError){ entry.date = 12 }
+		assert_raises(ArgumentError){ entry.date = '2014-4-31' }
 	end
 	
-	def test_set_amount
+	def test_set_revenue_expense
 		entry = Wallet::Entry.new
-		assert_equal(0, entry.amount)
+		assert_equal(0, entry.revenue)
+		assert_equal(0, entry.expense)
 		
-		entry = Wallet::Entry.new('2015-02-21', 20)
-		assert_equal(20, entry.amount)
+		entry = Wallet::Entry.new('test', '2015-02-21', 20)
+		assert_equal(20, entry.revenue)
+		assert_equal(0, entry.expense)
+		assert_equal(20, entry.balance)
 		
-		entry.amount = 24
-		assert_equal(24, entry.amount)
+		entry = Wallet::Entry.new('test', '2015-02-21', '20', '-21')
+		assert_equal(20, entry.revenue)
+		assert_equal(-21, entry.expense)
+		assert_equal(-1, entry.balance)
 		
-		entry.amount = 24.42
-		assert_equal(24.42, entry.amount)
+		entry.revenue = 24
+		entry.expense = -25
+		assert_equal(24, entry.revenue)
+		assert_equal(-25, entry.expense)
+		assert_equal(-1, entry.balance)
 		
-		entry = Wallet::Entry.new('2015-02-21', -42)
-		assert_equal(-42, entry.amount)
+		entry.revenue = 1.01
+		entry.expense = -1.02
+		assert_equal(1.01, entry.revenue)
+		assert_equal(-1.02, entry.expense)
+		assert_equal(-0.01, entry.balance)
 		
-		entry = Wallet::Entry.new('2015-02-21', -42.24)
-		assert_equal(-42.24, entry.amount)
+		entry.revenue = 1
+		entry.expense = -2.02
+		assert_equal(1, entry.revenue)
+		assert_equal(-2.02, entry.expense)
+		assert_equal(-1.02, entry.balance)
 		
-		assert_raises(ArgumentError){ entry.amount = 'string1' }
+		entry = Wallet::Entry.new('test', '2015-02-21', -42)
+		assert_equal(0, entry.revenue)
+		assert_equal(-42, entry.expense)
+		assert_equal(-42, entry.balance)
+		
+		entry = Wallet::Entry.new('test', '2015-02-21', -30.03)
+		assert_equal(0, entry.revenue)
+		assert_equal(-30.03, entry.expense)
+		assert_equal(-30.03, entry.balance)
+		
+		entry.revenue = 'string1'
+		assert_equal(0, entry.revenue)
+		
+		entry.expense = 'string1'
+		assert_equal(0, entry.expense)
+		
+		assert_raises(RangeError){ entry.revenue = -1.23 }
+		assert_raises(RangeError){ entry.expense = 1.23 }
 	end
 	
 	def test_set_category
-		entry = Wallet::Entry.new('2015-02-21', 20, 'c1')
+		entry = Wallet::Entry.new('test', '2015-02-21', 20, 0, 'c1')
 		assert_equal('c1', entry.category)
 		
 		entry.category = 'c2'
 		assert_equal('c2', entry.category)
+		
+		entry.category = 1.23
+		assert_equal('1.23', entry.category)
 	end
 	
 	def test_to_hash
-		entry = Wallet::Entry.new('2015-02-21', 20)
-		puts entry.to_h.to_s
+		entry = Wallet::Entry.new('test', '2015-02-21', 20, -12.34, 'c3')
+		
+		assert_equal('2015-02-21', entry.to_h['date'].to_s)
+		assert_equal(20, entry.to_h['revenue'])
+		assert_equal(-12.34, entry.to_h['expense'])
+		assert_equal(7.66, entry.to_h['balance'])
+		assert_equal('c3', entry.to_h['category'])
 	end
 end
