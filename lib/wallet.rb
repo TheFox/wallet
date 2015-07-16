aa7423b9d742ba7b476d3462165cf511823400fe
Christian Mayer <christian@fox21.at>
2015-07-16 16:39:35 +0200

    Gem config bugfix. #RN

--- lib/wallet.rb
+++ lib/wallet.rb
@@ -1,10 +1,4 @@
 
+require 'wallet/version'
 require 'wallet/wallet'
 require 'wallet/entry'
-
-module Wallet
-	NAME = 'Wallet'
-	VERSION = '0.5.0'
-	HOMEPAGE = 'https://github.com/TheFox/wallet'
-	NUMBER_FORMAT = '%.2f'
-end
---
+++ lib/wallet/version.rb
@@ -0,0 +1,7 @@
+
+module Wallet
+	NAME = 'Wallet'
+	VERSION = '0.5.1-dev'
+	HOMEPAGE = 'https://github.com/TheFox/wallet'
+	NUMBER_FORMAT = '%.2f'
+end
