93af96b54967f0e302ca3456b08daaad94d0b6cc
Christian Mayer <christian@fox21.at>
2015-12-21 23:19:20 +0100

    Version 0.8.2.

--- lib/wallet/version.rb
+++ lib/wallet/version.rb
@@ -2,7 +2,7 @@
 module TheFox
 	module Wallet
 		NAME = 'Wallet'
-		VERSION = '0.8.2-dev'
+		VERSION = '0.8.2'
 		DATE = '2015-12-21'
 		HOMEPAGE = 'https://github.com/TheFox/wallet'
 		NUMBER_FORMAT = '%.2f'
