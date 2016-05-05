a7bc5977eb22aeab7e2f1125c2f102b8a2c0caaf
Christian Mayer <christian@fox21.at>
2016-05-05 08:39:11 +0200

    Bugfix: show correct balance. #RN
    Got rid of the balance variable and sum revenue and expense on output.

--- lib/wallet/version.rb
+++ lib/wallet/version.rb
@@ -2,7 +2,7 @@
 module TheFox
 	module Wallet
 		NAME = 'Wallet'
-		VERSION = '0.9.2-dev.1'
+		VERSION = '0.9.2-dev.2'
 		DATE = '2016-05-05'
 		HOMEPAGE = 'https://github.com/TheFox/wallet'
 		NUMBER_FORMAT = '%.2f'
