7e7d4fd6e2e32c2ec82b37f01539baa0166e7f73
Christian Mayer <christian@fox21.at>
2016-07-04 14:22:57 +0200

    Version 0.10.1.

--- lib/wallet/version.rb
+++ lib/wallet/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module Wallet
 		NAME = 'Wallet'
-		VERSION = '0.10.0'
-		DATE = '2016-07-03'
+		VERSION = '0.10.1'
+		DATE = '2016-07-04'
 		HOMEPAGE = 'https://github.com/TheFox/wallet'
 		NUMBER_FORMAT = '%.2f'
 		NUMBER_ROUND = 5
