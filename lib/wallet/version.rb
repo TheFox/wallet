432eaabdeda51d500816f81b513e6dcb49097871
Christian Mayer <christian@fox21.at>
2016-10-15 22:56:32 +0200

    GitLab Deployment added.
    Closes #8.

--- lib/wallet/version.rb
+++ lib/wallet/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module Wallet
 		NAME = 'Wallet'
-		VERSION = '0.14.0-dev'
-		DATE = '2016-09-19'
+		VERSION = '0.14.0-dev.1'
+		DATE = '2016-10-16'
 		HOMEPAGE = 'https://github.com/TheFox/wallet'
 		
 		NUMBER_FORMAT = '%.2f'
