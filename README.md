f8330224dbe3043124698dc6fba757c22f54f95b
Christian Mayer <christian@fox21.at>
2016-10-20 15:15:56 +0200

    Version 0.14.0.

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ The preferred method of installation is via RubyGems.org:
 
 or via `Gemfile`:
 
-	gem 'thefox-wallet', '~>0.13'
+	gem 'thefox-wallet', '~>0.14'
 
 Use it in your sources:
 
--- lib/wallet/version.rb
+++ lib/wallet/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module Wallet
 		NAME = 'Wallet'
-		VERSION = '0.14.0-dev.2'
-		DATE = '2016-10-19'
+		VERSION = '0.14.0'
+		DATE = '2016-10-20'
 		HOMEPAGE = 'https://github.com/TheFox/wallet'
 		
 		NUMBER_FORMAT = '%.2f'
